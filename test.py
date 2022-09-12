105d92ea40676f8b7ac81c346a937262a6ff606b
drigger091 <42235258+drigger091@users.noreply.github.com>
2022-09-12 22:48:19 +0530

    update test.py
    included certain information

--- test.py
+++ test.py
@@ -10,9 +10,13 @@ response = requests.get("http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&
 print(response)
 #print(response.json())
 
-#iterating through the list
 
-for data in response.json():
-    print(data['title'])
-    print(data['link'])
-print()
+# channel ids for analysis
+#1 ted = UCAuUUnT6oDeKwE6v1NGQxug
+#2 carwow = UCUhFaUpnq31m6TNX2VKVSVA
+#3 DOUGMUNRO = UCsqjHFMB_JYTaEnf_vmTNqg
+#4 Motortrend = UCsAegdhiYLEoaFGuJFVrqFQ
+#5 Autocar india = UCjWs7BxyjO5SLqevxSmp4vQ
+#6 faisal Khan = UCPF4bAZimS4T8w1TlbeIAYg
+
+
